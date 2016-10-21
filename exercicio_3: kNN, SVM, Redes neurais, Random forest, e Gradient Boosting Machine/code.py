8930f1b57828a0375c5aefda3ab0f77ceef2de16
jbeleno <jsbeleno@gmail.com>
2016-10-21 15:23:59 -0200

    update: code done

--- exercicio_3: kNN, SVM, Redes neurais, Random forest, e Gradient Boosting Machine/code.py
+++ exercicio_3: kNN, SVM, Redes neurais, Random forest, e Gradient Boosting Machine/code.py
@@ -66,7 +66,7 @@ n_components_pca = 0
 knn_parameters = {'n_neighbors':[1, 5, 11, 15, 21, 25]}
 
 # parameters for SVM
-svm_parameters = {'kernel':('rbf'), 'C':[2**(-5), 2**(0), 2**(5), 2**(10)], 'gamma':[2**(-15), 2**(-10), 2**(-5), 2**(0), 2**(5)]}
+svm_parameters = {'kernel':['rbf'], 'C':[2**(-5), 2**(0), 2**(5), 2**(10)], 'gamma':[2**(-15), 2**(-10), 2**(-5), 2**(0), 2**(5)]}
 
 # Number of neurons in the hidden layer for Neural nets
 neural_nets_parameters = {'hidden_layer_sizes':[10, 20, 30, 40]}
@@ -76,6 +76,136 @@ random_forest_parameters = {'max_features':[10, 15, 20, 25], 'n_estimators':[100
 
 # Parameters for Gradient Boosting Machine
 gbm_parameters ={'learning_rate':[0.1, 0.05], 'max_depth':[5], 'n_estimators':[30, 70, 100]}
+
+
+# ------------------- Defining kNN with a % of the variance ------------------
+
+
+def get_precision_kNN_PCA(parameters, vp_pca, train_params, test_params, train_classes, test_classes, n_folds):
+    
+    # Applying the PCA keeping the variance over vp_pca %
+    pca = PCA(n_components = vp_pca)
+    pca.fit(train_params)
+    params_reduced_train = pca.transform(train_params)
+    params_reduced_test = pca.transform(test_params)
+        
+    # GridSearch over the kNN parameters using a 3 KFold
+    # The cv parameter is for Cross-validation
+    # We find the hyperparameters here
+    knn = KNeighborsClassifier()
+    clf_knn = GridSearchCV(knn, parameters, cv=n_folds)
+    clf_knn.fit(params_reduced_train, train_classes)
+    
+    # Getting the best hyperparameters
+    knn_best_hyperparams = clf_knn.best_params_
+    
+    # Create the best kNN model
+    knn_tuned = KNeighborsClassifier(n_neighbors=knn_best_hyperparams['n_neighbors'])
+    knn_tuned.fit(params_reduced_train, train_classes)
+    
+    # Get the precision of the model
+    knn_tuned_score = knn_tuned.score(params_reduced_test, test_classes)
+    
+    return knn_tuned_score
+    
+
+# ------------------------- SVM with kernel RBF -------------------------------
+
+
+def get_precision_svm(parameters, train_params, test_params, train_classes, test_classes, n_folds):
+    
+    # GridSearch over the SVM parameters using a 3 KFold
+    # The cv parameter is for Cross-validation
+    # We find the hyperparameters here
+    svm = SVC()
+    clf_svm = GridSearchCV(svm, parameters, cv=n_folds)
+    clf_svm.fit(train_params, train_classes)
+    
+    # Getting the best hyperparameters
+    svm_best_hyperparams = clf_svm.best_params_    
+    
+    # Create the best SVM model
+    svm_tuned = SVC(C = svm_best_hyperparams['C'], kernel = svm_best_hyperparams['kernel'], gamma = svm_best_hyperparams['gamma'])
+    svm_tuned.fit(train_params, train_classes)
+    
+    # Getting the model precision
+    svm_tuned_score = svm_tuned.score(test_params, test_classes)
+    
+    return svm_tuned_score
+
+# -------------------------- Neural Nets  -----------------------------------
+
+
+def get_precision_neural_nets(parameters, train_params, test_params, train_classes, test_classes, n_folds):
+    
+    # GridSearch over the Neural Nets parameters using a 3 KFold
+    # The cv parameter is for Cross-validation
+    # We find the hyperparameters here
+    nn = MLPClassifier()
+    clf_nn = GridSearchCV(nn, parameters, cv=n_folds)
+    clf_nn.fit(train_params, train_classes)
+    
+    # Getting the best hyperparameters
+    nn_best_hyperparams = clf_nn.best_params_    
+    
+    # Create the best Neural Net model
+    nn_tuned = MLPClassifier(hidden_layer_sizes = nn_best_hyperparams['hidden_layer_sizes'])
+    nn_tuned.fit(train_params, train_classes)
+    
+    # Getting the model precision
+    nn_tuned_score = nn_tuned.score(test_params, test_classes)
+    
+    return nn_tuned_score
+    
+
+# -------------------------- Neural Nets  -----------------------------------
+
+
+def get_precision_random_forest(parameters, train_params, test_params, train_classes, test_classes, n_folds):
+    
+    # GridSearch over the Random Forest parameters using a 3 KFold
+    # The cv parameter is for Cross-validation
+    # We find the hyperparameters here
+    rf = RandomForestClassifier()
+    clf_rf = GridSearchCV(rf, parameters, cv=n_folds)
+    clf_rf.fit(train_params, train_classes)
+    
+    # Getting the best hyperparameters
+    rf_best_hyperparams = clf_rf.best_params_    
+    
+    # Create the best Random Forest model
+    rf_tuned = RandomForestClassifier(max_features = rf_best_hyperparams['max_features'], n_estimators = rf_best_hyperparams['n_estimators'])
+    rf_tuned.fit(train_params, train_classes)
+    
+    # Getting the model precision
+    rf_tuned_score = rf_tuned.score(test_params, test_classes)
+    
+    return rf_tuned_score
+
+
+# -------------------------- Neural Nets  -----------------------------------
+
+
+def get_precision_gbm(parameters, train_params, test_params, train_classes, test_classes, n_folds):
+    
+    # GridSearch over the Grid Boosting parameters using a 3 KFold
+    # The cv parameter is for Cross-validation
+    # We find the hyperparameters here
+    gbm = GradientBoostingClassifier()
+    clf_gbm = GridSearchCV(gbm, parameters, cv=n_folds)
+    clf_gbm.fit(train_params, train_classes)
+    
+    # Getting the best hyperparameters
+    gbm_best_hyperparams = clf_gbm.best_params_    
+    
+    # Create the best Grid Boosting model
+    gbm_tuned = GradientBoostingClassifier(learning_rate = gbm_best_hyperparams['learning_rate'], max_depth = gbm_best_hyperparams['max_depth'], n_estimators = gbm_best_hyperparams['n_estimators'])
+    gbm_tuned.fit(train_params, train_classes)
+    
+    # Getting the model precision
+    gbm_tuned_score = gbm_tuned.score(test_params, test_classes)
+    
+    return gbm_tuned_score
 
 
 # -------------------------- Here goes the magic -----------------------------
@@ -109,33 +239,46 @@ for external_train_index, external_test_index in external_skf.split(df_parameter
     # Cleaning NaN for bad scaling
     # clean_external_params_train = np.nan_to_num(scaled_external_params_train)
     # clean_external_params_test = np.nan_to_num(scaled_external_params_test)
+     
     
-    # ************** Defining kNN with 80% of the variance ******************    
+    # Getting the kNN precision for this fold keeping PCA with 80% of the variance and internal CV 3-Fold
+    knn_score = get_precision_kNN_PCA(knn_parameters, variance_percentage_pca, scaled_external_params_train, scaled_external_params_test, external_classes_train, external_classes_test, n_internal_folds)
+    # 0.929769178069
     
-    # Applying the PCA keeping the variance over 80%
-    pca = PCA(n_components = variance_percentage_pca)
-    pca.fit(scaled_external_params_train)
-    external_params_reduced_train = pca.transform(scaled_external_params_train)
-    external_params_reduced_test = pca.transform(scaled_external_params_test)
-        
-    # GridSearch over the kNN parameters using a 3 KFold
-    # The cv parameter is for Cross-validation
-    # We find the hyperparameters here
-    knn_external = KNeighborsClassifier()
-    clf_knn_external = GridSearchCV(knn_external, knn_parameters, cv=n_internal_folds)
-    clf_knn_external.fit(external_params_reduced_train, external_classes_train)
+    # Getting the precision of SVM with kernel RBF using a 3-Fold internal CV
+    svm_score = get_precision_svm(svm_parameters, scaled_external_params_train, scaled_external_params_test, external_classes_train, external_classes_test, n_internal_folds)
+    # 0.93359083412    
     
-    # Getting the best hyperparameters
-    knn_best_hyperparams = clf_knn_external.best_params_
-    
-    # Create the best kNN model
-    knn_tuned = KNeighborsClassifier(n_neighbors=knn_best_hyperparams['n_neighbors'])
-    knn_tuned.fit(external_params_reduced_train, external_classes_train)
-    knn_tuned_score = knn_tuned.score(external_params_reduced_test, external_classes_test)
+    # Getting the precision of neural nets
+    neural_net_score = get_precision_neural_nets(neural_nets_parameters, scaled_external_params_train, scaled_external_params_test, external_classes_train, external_classes_test, n_internal_folds)
+    # 0.853934283295
+
+    # Getting the precision of random forest
+    random_forest_score = get_precision_random_forest(random_forest_parameters, scaled_external_params_train, scaled_external_params_test, external_classes_train, external_classes_test, n_internal_folds)
+    # 0.93359083412
+
+    # Getting the precision of gradient boosting
+    gbm_score = get_precision_gbm(gbm_parameters, scaled_external_params_train, scaled_external_params_test, external_classes_train, external_classes_test, n_internal_folds)
+    # 0.839945990058
     
     # Stacking the precision
-    knn_precision = knn_precision + knn_tuned_score
+    knn_precision = knn_precision + knn_score
+    svm_precision = svm_precision + svm_score
+    neural_net_precision = neural_net_precision + neural_net_score
+    random_forest_precision = random_forest_precision + random_forest_score
+    gbm_precision = gbm_precision + gbm_score
  
 knn_precision = knn_precision/n_external_folds
-print('Accuracy kNN: ', knn_precision)       
+svm_precision = svm_precision/n_external_folds
+neural_net_precision = neural_net_precision/n_external_folds
+random_forest_precision = random_forest_precision/n_external_folds
+gbm_precision = gbm_precision/n_external_folds
+
+print('Accuracy kNN: ', knn_precision)
+print('Accuracy SVM: ', svm_precision)
+print('Accuracy Neural Networks: ', neural_net_precision)
+print('Accuracy Random Forest: ', random_forest_precision)
+print('Accuracy Gradient Boosting: ', gbm_precision)
+
+
         
