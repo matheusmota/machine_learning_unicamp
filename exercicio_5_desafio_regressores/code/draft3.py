a7b0a24792fc810a50f2c84087376025763bc3bc
jbeleno <jsbeleno@gmail.com>
2016-11-07 17:18:20 -0200

    update: Work done!

--- exercicio_5_desafio_regressores/code/draft3.py
+++ exercicio_5_desafio_regressores/code/draft3.py
@@ -15,15 +15,15 @@ import math
 import numpy as np
 import pandas as pd
 
-from scipy.stats import randint as sp_randint
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import RandomizedSearchCV
 
 # ------------------------------- Getting Data -------------------------------
+
+output_file = '../results/values_test.csv'
 
 # Defining the URIs with raw data
 url_train_data = 'http://www.ic.unicamp.br/%7Ewainer/cursos/2s2016/ml/train.csv'
@@ -39,18 +39,30 @@ df_test = pd.read_csv(url_test_data, header = None, delimiter = ",")
 categorical_attributes = [4,5,6,7,8,9,11,12,15,16,17,20,22,28,29,30]
 general_le = []
 invert_index_le = 0
+general_le_test = []
+invert_index_le_test = 0
 
 train_params = df_train.iloc[:, 1:33]
 train_values = np.ravel(df_train.iloc[:, 1:2])
 
 df_train_with_numbers = df_train
+df_test_with_numbers = df_test
 
+# Training set
 for i in categorical_attributes:
     general_le.append(preprocessing.LabelEncoder())
     df_train_with_numbers[i] = general_le[invert_index_le].fit_transform(df_train_with_numbers[i])
     invert_index_le = invert_index_le + 1
     
 train_params_with_numbers = df_train_with_numbers.iloc[:, 1:33]
+
+# Test set
+for i in categorical_attributes:
+    general_le_test.append(preprocessing.LabelEncoder())
+    df_test_with_numbers[i-1] = general_le_test[invert_index_le_test].fit_transform(df_test_with_numbers[i-1])
+    invert_index_le_test = invert_index_le_test + 1
+    
+test_params_with_numbers = df_test_with_numbers
     
 
 # Number of columns and rows in the train data
@@ -67,6 +79,11 @@ n_external_folds = 3
 # Random Forest
 best_external_n_estimators = 64
 best_external_mae_rf = 1.0
+
+# Gradient Boosting Descent
+best_external_n_trees = 100
+best_learning_rate = 0.1
+best_external_mae_gbm = 1.0
 
 # WARNING: I work with an i5 with 4 cores 3.3.GHz, please adjust this parameter
 # to the number of cores your processor have
@@ -103,6 +120,35 @@ def rf_model(train_params, test_params, train_values, test_values):
             best_n_estimators = n_estimators
     
     return [best_mae, best_n_estimators]
+
+
+def gbm_model(train_params, test_params, train_values, test_values):
+    
+    learning_rate_array = [0.1, 0.05]
+    n_trees_array = [100, 125, 150]
+    
+    best_learning_rate = 0.1
+    best_n_trees = 100
+    best_mae = 1.0
+
+    for learning_rate in learning_rate_array:
+        for n_trees in n_trees_array:
+            
+            regressor = GradientBoostingRegressor(n_estimators = n_trees,
+                                                  criterion = 'mae',
+                                                  learning_rate = learning_rate)
+            regressor.fit(train_params, train_values)
+
+            model_predictions = regressor.predict(test_params)
+            mae = mean_absolute_error(test_values, model_predictions)
+
+            if mae < best_mae:
+                best_mae = mae
+                best_n_trees = n_trees
+                best_learning_rate = learning_rate
+    
+    return [best_mae, best_n_trees,best_learning_rate]
+
     
 # ------------------------ Here Goes the Magic -------------------------------
 
@@ -132,8 +178,37 @@ for external_train_index, external_test_index in external_skf.split(train_params
         best_external_mae_rf = rf_array[0]
         best_external_n_estimators = rf_array[1]
         
+    # Gradient  Boosting Descent
+    #gbm_array = gbm_model(external_params_train_with_numbers, 
+    #                      external_params_test_with_numbers, 
+    #                      external_classes_train, 
+    #                      external_classes_test)
+                          
+    # Using external cross-validation to fin best hyperparameters for GBM
+    #if gbm_array[0] < best_external_mae_gbm:
+    #    best_external_mae_gbm = gbm_array[0]
+    #    best_external_n_trees = gbm_array[1]
+    #    best_learning_rate = gbm_array[2]
+
+        
 
 print('Random Forest')
-print('Best # estimators: ', best_external_n_estimators)
+print('# of estimators: ', best_external_n_estimators)
 print('MAE: ', best_external_mae_rf)
-print("---------------------------------------------")
+print("---------------------------------")
+#print('Gradient Boosting Descent')
+#print('# of estimators: ', best_external_n_trees)
+#print('Learning rate: ', best_learning_rate)
+#print('MAE: ', best_external_mae_gbm)
+#print("---------------------------------")
+
+# ---------- Using the Random Forest Regressor on the test data -------------
+
+regressor = RandomForestRegressor(n_estimators = best_external_n_estimators,
+                                  criterion = 'mae',
+                                  n_jobs = n_jobs)
+regressor.fit(train_params_with_numbers, train_values)
+value_predictions = regressor.predict(test_params_with_numbers)
+
+df_predictions = pd.DataFrame(value_predictions)
+df_predictions.to_csv(output_file, sep=',', encoding='utf-8')
